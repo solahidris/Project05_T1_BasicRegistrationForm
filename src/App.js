e4c72b1a57954730b3317fdeeb57f3e878f16dda
solahidris <emailsolah@gmail.com>
2023-03-24 05:06:15 +0800

    added state for errors and validate function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
   const [userFirstName, setUserFirstName] = useState("");
   const [userLastName, setUserLastName] = useState("");
   const [userEmail, setUserEmail] = useState("");
+  const [errors, setErrors] = useState({
+    userFirstName: false,
+    userLastName: false,
+    userEmail: false,
+  });
 
   const firstNameHandler = (event) => {
     const firstName = event.target.value;
@@ -20,6 +25,21 @@ function App() {
     setUserEmail(email);
   };
 
+  const validateFields = () => {
+    const firstNameError = userFirstName.length < 5;
+    const lastNameError = userLastName.length < 5;
+    const emailError = !/\S+@\S+\.\S+/.test(userEmail);
+
+    setErrors({
+      userFirstName: firstNameError,
+      userLastName: lastNameError,
+      userEmail: emailError,
+    });
+
+    if (!(firstNameError || lastNameError || emailError)) {return}
+    // else {<span>something SPAN</span>};
+    // return !(firstNameError || lastNameError || emailError);
+  };
 
   return (
     <div className="App">
@@ -31,26 +51,44 @@ function App() {
           displayed for each field if validation fails else a success message is
           shown.
         </p>
-        <p>this is the userFirstName value = {userFirstName}</p>
-        <input
-          type={"text"}
-          value={userFirstName}
-          onChange={firstNameHandler}
-        />
-        <p>this is the userLastName value = {userLastName}</p>
-        <input
-          type={"text"}
-          value={userLastName}
-          onChange={lastNameHandler}
-        />
-        <p>this is the email value = {userEmail}</p>
-        <input
+        <label>
+          <p>UserFirstName = {userFirstName}</p>
+          <input
+            type={"text"}
+            value={userFirstName}
+            onChange={firstNameHandler}
+            // onBlur={validateFields}
+          />
+          {errors.userFirstName && (
+            <span> userFistName ERROR</span>
+          )}
+        </label>
+        <label>
+          <p>UserLastName = {userLastName}</p>
+          <input
+            type={"text"}
+            value={userLastName}
+            onChange={lastNameHandler}
+            // onBlur={validateFields}
+          />
+          {errors.userLastName && <span> userLastName ERROR</span>}
+        </label>
+        <label>
+          <p>UserEmail = {userEmail}</p>
+          <input
           type={"email"}
           value={userEmail}
           onChange={emailHandler}
-        />
+          // onBlur={validateFields}
+          />
+          {errors.userEmail && <span> userEmail ERROR</span>}
+        </label>
         <p>----</p>
-        <button onClick={()=>{console.log([userFirstName, userLastName, userEmail])}}>submit</button>
+        <button
+          onClick={(validateFields)}
+        >
+          submit
+        </button>
         <p className="projectTitle">
           build a basic registration form in React where users can enter first
           name, last name, and email. Post-registration, an error message is
